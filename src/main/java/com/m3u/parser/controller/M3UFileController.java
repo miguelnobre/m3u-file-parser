bda7e231fe076ac609d94c78fb92ef919b008ff4
Emanuel Teixeira <eteixeira11@hotmail.com>
2020-05-02 14:36:52 +0200

    Added some syntax sugar to controller

--- src/main/java/com/m3u/parser/controller/M3UFileController.java
+++ src/main/java/com/m3u/parser/controller/M3UFileController.java
@@ -5,15 +5,17 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.HttpHeaders;
+import java.io.FileInputStream;
 import java.net.URL;
-import java.nio.file.Files;
 import java.util.Set;
 
 @RestController
@@ -27,16 +29,17 @@ public class M3UFileController {
     @SneakyThrows
     @GetMapping("download")
     @ApiOperation(value = "Download M3U file", notes = "Filter and download m3u file")
-    public void download(@RequestParam String fileUrl,
-                         @RequestParam(required = false, defaultValue = "") Set<String> categoryFilter,
-                         HttpServletResponse response) {
+    //using raw type because swagger doc doesn't support InputStreamResource
+    public ResponseEntity download(@RequestParam String fileUrl,
+                                   @RequestParam(required = false, defaultValue = "") Set<String> categoryFilter) {
 
-        this.m3UDownloadFileService.downloadFile(new URL(fileUrl), categoryFilter)
-                .subscribe(file -> {
-                    response.addHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=etc.m3u");
-                    Files.copy(file.toPath(), response.getOutputStream());
-                    response.getOutputStream().flush();
-                }).dispose();
+        return this.m3UDownloadFileService.downloadFile(new URL(fileUrl), categoryFilter)
+                .map(file -> ResponseEntity.ok()
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=etc.m3u")
+                        .contentLength(file.length())
+                        .contentType(MediaType.parseMediaType("application/pdf"))
+                        .body(new InputStreamResource(new FileInputStream(file)))
+                ).blockingGet();
 
     }
 }
